678778fcf2a258e1c8fbdfa91565ad4482480926
Liz Myers <elizmyers@gmail.com>
2024-08-03 00:58:17 -0400

    test globals css

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 import { ClerkProvider } from '@clerk/nextjs';
 import { Toaster } from '@/components/ui/toaster';
-import "./global.css";
+import "./globals.css";
 
 export default function RootLayout({
   children,
